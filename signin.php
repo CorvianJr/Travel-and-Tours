fde0c90d6ac9dc64600a175ad7b5900bd47e11b2
CorvianJr <adrielallenpenaflor@gmail.com>
2024-05-01 20:18:10 +0800

    Signin, client, and login php (Not working yet)

--- signin.php
+++ signin.php
@@ -1,3 +1,18 @@
+<!-- HTML Login Page -->
+<?php
+    //session means it saves a data in the system
+    session_start();
+
+    //if session is set, then it means an account was logged in
+    if(isset($_SESSION['username'])){
+        if($_SESSION['userlvl']=='admin'){
+            header("location: admin/admin.php");
+        }else{
+            header("location: client.php");
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -46,6 +61,7 @@
         <div class="container-fluid">
             <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
                 <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                    <form action="" id="loginform">
                     <div class="bg-secondary rounded p-4 p-sm-5 my-4 mx-3">
                         <div class="d-flex align-items-center justify-content-between mb-3">
                            <!-- <a href="index.html" class="">
@@ -54,28 +70,83 @@
                             <h3>Log In</h3>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+                            <input type="username" class="form-control" id="username" name="username">
+                            <label for="floatingInput">Username</label>
                         </div>
                         <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <input type="password" class="form-control" name="password" id="password" required>
                             <label for="floatingPassword">Password</label>
                         </div>
                         <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Remember Me</label>
-                            </div>
+                        <div>
+                        <h6 class="text-light"><input type="checkbox" onclick="revealPsw()">  Show Password</h6>
+                        </div>      
                             <a href="">Forgot Password</a>
                         </div>
                         <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Log In</button>
                         <p class="text-center mb-0">Don't have an Account? <a href="signup.html">Sign Up</a></p>
                     </div>
+                </form>
                 </div>
             </div>
         </div>
         <!-- Sign In End -->
     </div>
+    <script>
+        //reveal password function 
+        function revealPsw() {
+            var x = document.getElementById("floatingPassword");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+    </script>
+
+    <!-- modal -->
+        <!-- failed -->
+        <div class="modal fade" role="dialog" id="error">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content rounded-4 shadow bg-danger-subtle">
+                <div class="modal-header border-bottom-0">
+                    <h1 class="modal-title fs-5 text-light">Invalid User</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body py-0">
+                    <p class="text-light">Invalid credentials!</p>
+                </div>
+                </div>
+            </div>
+        </div>
+
+      
+                
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js" integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            $(document).ready(function(){
+                //login form submit
+                $("#loginform").submit(function(e){
+                    e.preventDefault();
+                    //get data
+                    $uname = $("#username").val();
+                    $password = $("#password").val();
+                    //$cred = Somehow has to get the userlvl
+                    $.post("login.php",{uname:$uname, pass:$password},function(data){ 
+                        //change $uname to userlvl, just testing
+                       if($uname == 'client'){ // if the response is client redirect to index dashboard
+                        window.location.href = "client/client.php";
+                       }else if($uname == 'admin'){  // if the response is admin redirect to admin dashboard
+                        window.location.href = "admin/admin.php";
+                       }else{ //if none then show modal alert
+                        $("#error").modal("show");
+                       }
+                    });
+                });
+            });
+        </script>
+       
 
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
