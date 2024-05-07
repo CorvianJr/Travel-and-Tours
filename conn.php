5955b410b3152472556571aa39fa85d4eebce2b9
Vect0 <151480608+Vect0@users.noreply.github.com>
2024-05-07 13:19:35 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,25 +1,7 @@
 <?php
-
-    // Database connection parameters
-    $hostname = 'localhost';
-    $database = 'db_ttms';
-    $username = 'root';
-    $pass = '';
-    
-    
-
-    try {
-        // Create a PDO connection to the database
-        $conn = new PDO('mysql:host=$hostname;dbname=$database', $username, $pass);
-
-        // Set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        // Output success message if connected successfully (optional)
-        // echo "Connected successfully";
-    } catch(PDOException $e) {
-        // Output error message if connection fails
-        echo "Connection failed: " . $e->getMessage();
+    $conn = new PDO('mysql:host=localhost;dbname=db_ttms','root','');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if (!$conn) {
+        die("Connection Error") . $mysql->connect_errno;
     }
-   
 ?>
