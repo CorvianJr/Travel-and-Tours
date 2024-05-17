482d54724a08a994b9b35b3fa9b10b0adc1c3dcb
CorvianJr <adrielallenpenaflor@gmail.com>
2024-05-17 21:32:44 +0800

    Session attempt

--- client.php
+++ client.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -142,12 +146,12 @@
                     <div class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">
                             <img class="rounded-circle me-lg-2" src="img/user.jpg" alt="" style="width: 40px; height: 40px;">
-                            <span class="d-none d-lg-inline-flex"><?php echo session_id(); ?></span>
+                            <span class="d-none d-lg-inline-flex"><?php echo $_SESSION['username']; ?></span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end bg-secondary border-0 rounded-0 rounded-bottom m-0">
                             <a href="#" class="dropdown-item">My Profile</a>
                             <a href="#" class="dropdown-item">Settings</a>
-                            <a href="nouser.html" class="dropdown-item">Log Out</a>
+                            <a href="index.php" class="dropdown-item">Log Out</a>
                         </div>
                     </div>
                 </div>
